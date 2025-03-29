e6503ff683c9d7b54db704e2535b43345e46496f
Servan42 <charlot.servan@gmail.com>
2025-03-29 01:32:13 +0100

    feat: Improved GA

--- SudokuSolver/Solver.cs
+++ SudokuSolver/Solver.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace SudokuSolver
 {
@@ -12,10 +7,12 @@ namespace SudokuSolver
     {
         private readonly Random random;
 
-        const int POPULATION_SIZE = 1000;
-        const float percentageOfElitism = 0.1f;
-        const float percentageOfParentsToKeep = 0.5f;
-        const float percentageOfGeneMutationChange = 0.1f;
+        const int POPULATION_SIZE = 2000;
+        const float PRECENTAGE_OF_ELITISM = 0.01f;
+        const float PERCENTAGE_OF_PARENTS_TO_KEEP = 1.0f;
+        const int CANDIDATES_NB_IN_TOURNAME = 3;
+        const float PERCENTAGE_OF_OFFSPRING_MUTATION_CHANCE = 0.5f;
+        float PERCENTAGE_OF_GENE_MUTATION_CHANCE = 0.1f;
 
         // Cached sudoku data
         private string[] templateGrid;
@@ -37,58 +34,89 @@ namespace SudokuSolver
             GenerateInitialPopulation();
 
             Sudoku? winner = null;
+            (int lastAverage, int occurences) localMaxBreaker = (0, 0);
             do
             {
                 generationCount++;
-                var newGeneration = new List<Sudoku>();
                 this.population = this.population.OrderByDescending(x => x.Score).ToList();
-                newGeneration.AddRange(SelectElite());
-                population = BreedAndMutate(newGeneration);
+
+                int avg = (int) Math.Floor(population.Average(x => x.Score));
+                if(avg == localMaxBreaker.lastAverage)
+                    localMaxBreaker.occurences++;
+                else
+                    localMaxBreaker = (avg, 0);
+
+                if (localMaxBreaker.occurences > 20)
+                {
+                    Console.WriteLine($"Population reset at Gen: {generationCount,4} | Avg: {avg,3} | Max: {population[0].Score,3}");
+                    population.Clear();
+                    GenerateInitialPopulation();
+                    generationCount = 0;
+                }
+
+                population = BreedAndMutate();
                 winner = population.FirstOrDefault(x => x.Score == Sudoku.WINNING_SCORE);
 
-                Console.WriteLine($"Generation {generationCount} | Score: {population.Average(x => x.Score)}");
             } while (winner == null);
 
-            Console.WriteLine($"Found after {generationCount} generations");
             return winner;
         }
 
         private IEnumerable<Sudoku> SelectElite()
         {
             return this.population
-                .Take((int)(POPULATION_SIZE * percentageOfElitism));
+                .Take((int)(POPULATION_SIZE * PRECENTAGE_OF_ELITISM));
+                //.DistinctBy(x => x.FillingString);
         }
 
         private IEnumerable<Sudoku> SelectParents()
         {
             return this.population
-                .Take((int)(POPULATION_SIZE * percentageOfParentsToKeep));
+                .Take((int)(POPULATION_SIZE * PERCENTAGE_OF_PARENTS_TO_KEEP));
         }
 
-        private List<Sudoku> BreedAndMutate(List<Sudoku> newGeneration)
+        private List<Sudoku> BreedAndMutate()
         {
-            var parents = new List<Sudoku>(SelectParents());
-            while(newGeneration.Count < POPULATION_SIZE)
+            var newGeneration = new List<Sudoku>();
+
+            newGeneration.AddRange(SelectElite());
+
+            var eligibleParents = new List<Sudoku>(SelectParents());
+            while (newGeneration.Count < POPULATION_SIZE)
             {
-                var parent1 = population[random.Next(0, parents.Count)].FillingString;
-                var parent2 = population[random.Next(0, parents.Count)].FillingString;
+                var parent1 = TournamentSelection(eligibleParents);
+                var parent2 = TournamentSelection(eligibleParents);
                 var offspring = Breed(parent1, parent2);
                 offspring = Mutate(offspring);
                 var sudoku = new Sudoku(templateGrid);
                 sudoku.Fill(offspring);
-                sudoku.Evaluate();
+                //sudoku.Evaluate();
                 newGeneration.Add(sudoku);
             }
 
+            Parallel.ForEach(newGeneration, sudoku => { sudoku.Evaluate(); });
             return newGeneration;
+        }
+
+        private string TournamentSelection(List<Sudoku> eligibleParents)
+        {
+            var tournament = new List<Sudoku>();
+            for (int i = 0; i < Math.Min(CANDIDATES_NB_IN_TOURNAME, eligibleParents.Count); i++)
+            {
+                tournament.Add(eligibleParents[random.Next(0, eligibleParents.Count)]);
+            }
+            return tournament.OrderByDescending(x => x.Score).First().FillingString;
         }
 
         private string Mutate(string offspring)
         {
+            if(random.NextSingle() > PERCENTAGE_OF_OFFSPRING_MUTATION_CHANCE)
+                return offspring;
+
             var mutatedOffspring = offspring.ToArray();
             for (int i = 0; i < mutatedOffspring.Length; i++)
             {
-                if (random.NextSingle() > percentageOfGeneMutationChange)
+                if (random.NextSingle() > PERCENTAGE_OF_GENE_MUTATION_CHANCE)
                     continue;
 
                 mutatedOffspring[i] = random.CharNum();
